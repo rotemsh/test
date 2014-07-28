5fe23887797633ecc0dec873e7eb4c0593725e01
omiRots <omirots@gmail.com>
2014-07-28 19:17:04 +0300

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Rotem Shukron on 7/28/14.
+//  Copyright (c) 2014 Rotem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Rotem Shukron on 7/28/14.
+//  Copyright (c) 2014 Rotem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
