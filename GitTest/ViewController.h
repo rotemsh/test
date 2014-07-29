32a74a01770a0d9369b04e3b22af0e48abb4ead8
rotemsh <rotem.shukron@gmail.com>
2014-07-29 12:18:51 +0300

    Checking

--- GitTest/ViewController.h
+++ GitTest/ViewController.h
@@ -10,9 +10,12 @@
 #import <FacebookSDK/FacebookSDK.h>
 
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <FBLoginViewDelegate>
 
-@property (weak, nonatomic) IBOutlet FBLoginView *loginView;
+@property (strong, nonatomic) IBOutlet FBLoginView *loginView;
 
+@property (strong, nonatomic) IBOutlet FBProfilePictureView *profilePictureView;
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *statusLabel;
 
 @end
--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -29,6 +29,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user
+{
+    self.profilePictureView.profileID = user.objectID;
+    self.nameLabel.text = user.name;
+}
 
+-(void)loginViewShowingLoggedInUser:(FBLoginView *)loginView
+{
+    self.statusLabel.text = @"You're Logged in as";
+    
+}
+
+-(void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView
+{
+//    self.profilePictureView.profileID = nil;
+    self.nameLabel.text = @"";
+    self.statusLabel.text = @"You're not logged in! :(";
+}
 
 @end
